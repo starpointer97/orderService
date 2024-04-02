85dd2e9ba5fdad65b25dad5c4bc9e13da09ec7f7
Nishit Patira <npatira@amazon.com>
2024-04-03 01:48:37 +0530

    Adding write trades functionality

--- src/main/java/com/nishit/bitvavo/io/writers/StandardWriter.java
+++ src/main/java/com/nishit/bitvavo/io/writers/StandardWriter.java
@@ -1,11 +1,22 @@
 package com.nishit.bitvavo.io.writers;
 
 import com.nishit.bitvavo.beans.OrderBook;
+import com.nishit.bitvavo.beans.Trade;
+
+import java.util.List;
 
 public class StandardWriter implements Writer {
 
     @Override
     public void writeOrderBook(OrderBook orderBook) {
         //TODO : Implement after getting clarity from Bitvavo on output format.
+    }
+
+    @Override
+    public void writeTrades(List<Trade> tradeList) {
+        for(Trade trade : tradeList){
+            System.out.println(trade);
+        }
+
     }
 }
--- src/main/java/com/nishit/bitvavo/io/writers/Writer.java
+++ src/main/java/com/nishit/bitvavo/io/writers/Writer.java
@@ -1,8 +1,13 @@
 package com.nishit.bitvavo.io.writers;
 
 import com.nishit.bitvavo.beans.OrderBook;
+import com.nishit.bitvavo.beans.Trade;
+
+import java.util.List;
 
 public interface Writer {
 
     public void writeOrderBook(OrderBook orderBook);
+
+    public void writeTrades(List<Trade> tradeList);
 }
