fd53c93b08c59d479ef274588a6364dfb4d5a8d2
Nishit Patira <npatira@amazon.com>
2024-04-06 18:29:46 -0700

    MD5 parser for testing the output

--- src/main/java/com/nishit/bitvavo/utils/MD5Parser.java
+++ src/main/java/com/nishit/bitvavo/utils/MD5Parser.java
@@ -1,2 +1,26 @@
-package com.nishit.bitvavo.utils;public class MD5Parser {
+package com.nishit.bitvavo.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Parser {
+
+    public static String getMD5Hash(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            md.update(input.getBytes());
+
+            byte[] bytes = md.digest();
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("MD5 algorithm not available");
+            return null;
+        }
+    }
 }
