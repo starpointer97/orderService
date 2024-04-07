f1324342b2105f2afec8e1d7f1c0eb3b17a92cbf
Nishit Patira <npatira@amazon.com>
2024-04-06 18:30:55 -0700

    Completing the main method for testing via CLI

--- src/main/java/com/nishit/bitvavo/app/AppLauncher.java
+++ src/main/java/com/nishit/bitvavo/app/AppLauncher.java
@@ -10,27 +10,36 @@ import com.nishit.bitvavo.io.writers.Writer;
 import com.nishit.bitvavo.io.writers.WriterFactory;
 import com.nishit.bitvavo.matcher.MatchingEngine;
 import com.nishit.bitvavo.parser.CSVParser;
+import com.nishit.bitvavo.utils.MD5Parser;
 
 import java.util.List;
 import java.util.Optional;
 
 public class AppLauncher {
-
     public static void main(String[] args) {
         Reader inputReader = ReaderFactory.getReader(IOMode.STANDARD);
         Writer outputWriter = WriterFactory.getWriter(IOMode.STANDARD);
         MatchingEngine matchingEngine = MatchingEngine.getMatchingEngine();
-        while(true){
+        StringBuilder outputBuilder = new StringBuilder();
+        while (true) {
             Optional<String> input = inputReader.nextLine();
-            if(input.isPresent()){
+            if (input.isPresent()) {
                 Order incomingOrder = CSVParser.parseCSVInput(input.get());
                 List<Trade> tradeList = matchingEngine.addAndExecuteOrder(incomingOrder);
-                outputWriter.writeTrades(tradeList);
-            } else {
+                String tradesForThisAggressingOrder = outputWriter.writeTrades(tradeList);
+                outputBuilder.append(tradesForThisAggressingOrder);
+                System.out.print(tradesForThisAggressingOrder);
+            }
+            else {
                 break;
             }
         }
+        inputReader.closeStream();
         OrderBook orderBook = matchingEngine.getOrderBook();
-        outputWriter.writeOrderBook(orderBook);
+        String orderBookAfterAllTrades = outputWriter.writeOutOrderBook(orderBook);
+        outputBuilder.append(orderBookAfterAllTrades);
+        System.out.println(orderBookAfterAllTrades);
+
+        System.out.println(MD5Parser.getMD5Hash(outputBuilder.toString()));
     }
 }
